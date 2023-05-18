57a4b9dbdfda322c3b2d5ff600fb12b88e95d4fc
Alex <alexdeividy@hotmail.com>
2023-05-18 20:27:31 -0300

    Novas dúvidas

--- projetoIndividual.py
+++ projetoIndividual.py
@@ -1,43 +1,38 @@
-candidato1 = ["e8_t8_p8_s8"]
-alex = " ".join(candidato1)
-#---------------------------------#
-candidato2 = ["e7_t7_p7_s7"]
-joao = " ".join(candidato2)
-#---------------------------------#
-candidato3 = ["e6_t6_p6_s6"]
-pedro = " ".join(candidato3)
-#---------------------------------#
-candidato4 = ["e5_t5_p5_s5"]
-lucas = " ".join(candidato4)
-#---------------------------------#
-candidato5 = ["e9_t9_p9_s9"]
-rafael = " ".join(candidato5)
 
-listaVazia = []
+candidato1 = ["e5_t10_p8_s8"]
+candidato2 = ["e10_t7_p7_s8"]
+candidato3 = ["e8_t5_p4_s9"]
+candidato4 = ["e2_t2_p2_s1"]
+candidato5 = ["e10_t10_p8_s9"]
+
+listaAprovados = []
+
 n1 = 0
 n2 = 0
 n3 = 0
 n4 = 0
 
-usuarioE = input("Digite a nota minima para a Entrevista: ")
-usuarioT = input("Digite a nota minima para o Teste Teórico: ")
-usuarioP = input("Digite a nota minima para o Teste Prático: ")
-usuarioS = input("Digite a nota minima para as Soft Skills: ")
+usuarioE = int(input("Digite a nota minima para a Entrevista: "))
+usuarioT = int(input("Digite a nota minima para o Teste Teórico: "))
+usuarioP = int(input("Digite a nota minima para o Teste Prático: "))
+usuarioS = int(input("Digite a nota minima para as Soft Skills: "))
 
-def projetoN(n1, n2, n3, n4, notas):
+def filtraUsuarios(n1, n2, n3, n4, notas):
 
-    n1 = notas[1]
-    n2 = notas[4]
-    n3 = notas[7]
-    n4 = notas[10]
+    candidato = " ".join(notas)
+    n1, n2, n3, n4 = candidato.split("_")
+    n1 = int(n1[1:])
+    n2 = int(n2[1:])
+    n3 = int(n3[1:])
+    n4 = int(n4[1:])
 
     if n1 >= usuarioE and n2 >= usuarioT and n3 >= usuarioP and n4 >= usuarioS:
-        listaVazia.append(notas)
-    
-projetoN(n1, n2, n3, n4, alex)
-projetoN(n1, n2, n3, n4, joao)
-projetoN(n1, n2, n3, n4, pedro)
-projetoN(n1, n2, n3, n4, lucas)
-projetoN(n1, n2, n3, n4, rafael)
+        listaAprovados.append(candidato)
 
-print(listaVazia)
+filtraUsuarios(n1, n2, n3, n4, candidato1)
+filtraUsuarios(n1, n2, n3, n4, candidato2)
+filtraUsuarios(n1, n2, n3, n4, candidato3)
+filtraUsuarios(n1, n2, n3, n4, candidato4)
+filtraUsuarios(n1, n2, n3, n4, candidato5)
+
+print(listaAprovados)

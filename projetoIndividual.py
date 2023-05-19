a88f78ebb63f7834698ba9c2ba1ed4ae81e3e0c5
Alex <alexdeividy@hotmail.com>
2023-05-18 21:18:18 -0300

    Melhorias de código

--- projetoIndividual.py
+++ projetoIndividual.py
@@ -1,3 +1,14 @@
+def filtraCandidatos(n1, n2, n3, n4, notas):
+
+    candidato = " ".join(notas)
+    n1, n2, n3, n4 = candidato.split("_")
+    n1 = int(n1[1:])
+    n2 = int(n2[1:])
+    n3 = int(n3[1:])
+    n4 = int(n4[1:])
+
+    if n1 >= usuarioE and n2 >= usuarioT and n3 >= usuarioP and n4 >= usuarioS:
+        listaAprovados.append(notas)
 
 candidato1 = ["e5_t10_p8_s8"]
 candidato2 = ["e10_t7_p7_s8"]
@@ -17,22 +28,22 @@ usuarioT = int(input("Digite a nota minima para o Teste Teórico: "))
 usuarioP = int(input("Digite a nota minima para o Teste Prático: "))
 usuarioS = int(input("Digite a nota minima para as Soft Skills: "))
 
-def filtraUsuarios(n1, n2, n3, n4, notas):
+filtraCandidatos(n1, n2, n3, n4, candidato1)
+filtraCandidatos(n1, n2, n3, n4, candidato2)
+filtraCandidatos(n1, n2, n3, n4, candidato3)
+filtraCandidatos(n1, n2, n3, n4, candidato4)
+filtraCandidatos(n1, n2, n3, n4, candidato5)
 
-    candidato = " ".join(notas)
-    n1, n2, n3, n4 = candidato.split("_")
-    n1 = int(n1[1:])
-    n2 = int(n2[1:])
-    n3 = int(n3[1:])
-    n4 = int(n4[1:])
+if candidato1 in listaAprovados:
+    print("Candidato 1 ", candidato1)
+elif candidato2 in listaAprovados:
+    print("Candidato 2 ", candidato2)
+elif candidato3 in listaAprovados:
+    print("Candidato 3 ", candidato3)
+elif candidato4 in listaAprovados:
+    print("Candidato 4 ", candidato4)
+elif candidato5 in listaAprovados:
+    print("Candidato 5 ", candidato5)
+else:
+    print("Nenhum candidato aprovado!")
 
-    if n1 >= usuarioE and n2 >= usuarioT and n3 >= usuarioP and n4 >= usuarioS:
-        listaAprovados.append(candidato)
-
-filtraUsuarios(n1, n2, n3, n4, candidato1)
-filtraUsuarios(n1, n2, n3, n4, candidato2)
-filtraUsuarios(n1, n2, n3, n4, candidato3)
-filtraUsuarios(n1, n2, n3, n4, candidato4)
-filtraUsuarios(n1, n2, n3, n4, candidato5)
-
-print(listaAprovados)

1f2713584bf6bbd29ecfc46eaf0fa3a1b44ddd97
Alex <alexdeividy@hotmail.com>
2023-05-26 09:40:39 -0300

    Readme Atualizado

--- README.md
+++ README.md
@@ -1,4 +1,23 @@
 # Projeto Individual 2
- Projeto individual 2 - Resilia Educação
+ Projeto individual 2 - Resilia Educação - Candidatos aprovados!
 
- Criar um código que filtre os candidatos por suas notas de acordo com as entradas do usuário
+ Objetivo:
+
+    A partir de 5 listas, cada uma com 1 candidato, é necessário fazer a filtragem dos mesmos,
+    de acordo com os critérios(notas mínimas) definidos pela entrada do usuário.
+
+ Foram utilizados:
+
+    Uma função, que irá filtrar os candidatos de acordo com as notas digitadas pelo usuário, dentro dela teremos:
+        Método split, para selecionar as notas de cada danditato e utiliza-lá fora da lista.
+        E um IF, para mandar os candidatos aprovados para uma lista previamente criada vazia.
+    
+    listas predefinidas com as notas do usuário em formato String.
+    Inputs para a inserção das notas que o usuário deseja.
+    Cadeia de Ifs para printar por vez, cada usuário contendo na lista de aprovados, e Else caso nenhum seja aprovado.
+
+
+
+ Linguagem: Python 3.11.2
+ Editor: VSCode
+ Feito por: Alex Santana
--- projetoIndividual2.py
+++ projetoIndividual2.py
@@ -1,39 +1,48 @@
-def filtraCandidatos(n1, n2, n3, n4, notas):
+#Funcão que irá filtrar os candidatos de acordo com as notas digitadas pelo usuário
+#Utilizando split, cada nota é selecionada de acordo com seu posicionamento na String, pulando os "_" e a partir do indice 1
+#If para verificar e adicionar na lista de aprovados apenas os candidatos que passarem nos requesitos mínimos
+def filtraCandidatos(entrevista, testeTeorico, testePratico, softSkills, notas):
 
     candidato = " ".join(notas)
-    n1, n2, n3, n4 = candidato.split("_")
-    n1 = int(n1[1:])
-    n2 = int(n2[1:])
-    n3 = int(n3[1:])
-    n4 = int(n4[1:])
+    entrevista, testeTeorico, testePratico, softSkills = candidato.split("_")
+    entrevista = int(entrevista[1:])
+    testeTeorico = int(testeTeorico[1:])
+    testePratico = int(testePratico[1:])
+    softSkills = int(softSkills[1:])
 
-    if n1 >= usuarioE and n2 >= usuarioT and n3 >= usuarioP and n4 >= usuarioS:
+    if entrevista >= usuarioE and testeTeorico >= usuarioT and testePratico >= usuarioP and softSkills >= usuarioS:
         listaAprovados.append(notas)
 
+#Lista de candidatos
 candidato1 = ["e5_t10_p8_s8"]
 candidato2 = ["e10_t7_p7_s8"]
 candidato3 = ["e8_t5_p4_s9"]
 candidato4 = ["e2_t2_p2_s1"]
 candidato5 = ["e10_t10_p8_s9"]
 
+#Lista de canditatos aprovados (é preenchida de acordo com o andamento da função (filtraCandidatos))
 listaAprovados = []
 
-n1 = 0
-n2 = 0
-n3 = 0
-n4 = 0
+#Variáveis vazias das notas dos candidatos, que são chamadas em conjunto da função (filtraCandidatos)
+entrevista = 0
+testeTeorico = 0
+testePratico = 0
+softSkills = 0
 
+#Inputs para o usuário inserir as notas desejadas
 usuarioE = int(input("Digite a nota mínima da Entrevista: "))
 usuarioT = int(input("Digite a nota mínima do Teste Teórico: "))
 usuarioP = int(input("Digite a nota mínima do Teste Prático: "))
 usuarioS = int(input("Digite a nota mínima de Soft Skills: "))
 
-filtraCandidatos(n1, n2, n3, n4, candidato1)
-filtraCandidatos(n1, n2, n3, n4, candidato2)
-filtraCandidatos(n1, n2, n3, n4, candidato3)
-filtraCandidatos(n1, n2, n3, n4, candidato4)
-filtraCandidatos(n1, n2, n3, n4, candidato5)
+#Chamada da função filtraCandidatos
+filtraCandidatos(entrevista, testeTeorico, testePratico, softSkills, candidato1)
+filtraCandidatos(entrevista, testeTeorico, testePratico, softSkills, candidato2)
+filtraCandidatos(entrevista, testeTeorico, testePratico, softSkills, candidato3)
+filtraCandidatos(entrevista, testeTeorico, testePratico, softSkills, candidato4)
+filtraCandidatos(entrevista, testeTeorico, testePratico, softSkills, candidato5)
 
+#Pilha de ifs para printar cada candidato da lista, de acordo com o seu número no cadastro
 if candidato1 in listaAprovados:
     print("Candidato 1 ", candidato1)
 if candidato2 in listaAprovados:
